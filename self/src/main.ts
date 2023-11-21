7fa5197cb3397663d29a8541de5736ce16d8f948
Samsonium <tdesu@vk.com>
2023-11-21 15:15:48 +0300

    Настроен Swagger

--- self/src/main.ts
+++ self/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+      .setTitle('K-Space API — Self')
+      .setDescription('API сервиса авторизации и аутентификации')
+      .setVersion('1.0.0')
+      .build();
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/docs', app, swaggerDoc);
+
+  await app.listen(7000);
 }
-bootstrap();
+bootstrap().then();
